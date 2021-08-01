2939b1b6b86c7aac112cf27d5d070753540f69b4
Yog <yog.panjarale@gmail.com>
2021-08-01 23:48:22 +0530

    just performace test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// extern crate big_unsigned_ints};
+
 fn main() {
-    println!("Hello, world!");
+    // let n = big_unsigned_ints::U2048::new();
+    let mut n :i128= 2;
+    n= n.pow(68);
+    println!("{}", n);
+    let mut i:i128 = 0;
+    while i < n{
+        if i%1_00_000_000==0{
+            print!("{} ",i);
+        }
+        i+=1;
+    }
 }
+//295147905179352825856
+//16:47:10 0
+//16:54:30 13105000000
